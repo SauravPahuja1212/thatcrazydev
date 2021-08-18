c2ebd2ffb4a93cebeb30609100240d946b0a1e17
SauravPahuja1212 <sauravpahuja1995@gmail.com>
2021-08-18 17:26:19 +0530

    Initial Commit

---
+++ javascript/script.js
@@ -0,0 +1,14 @@
+const animate = () => {
+    let flag = document.getElementById('flag');
+    flag.classList.toggle('curve-visible');
+}
+
+const toggleNav = () => {
+    let sidenav = document.getElementById('sidenav');
+    sidenav.classList.toggle('sidenav-visible');
+}
+
+let links = document.getElementsByClassName('sidenav');
+for(let a of links[0].children) {
+    a.addEventListener("click", toggleNav);
+}
